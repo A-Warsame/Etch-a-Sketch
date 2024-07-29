c8338e0c4cf05133d1f8ff7520ba6021309b39e5
A-Warsame <aawarsame12@gmail.com>
2024-07-29 13:37:27 +0100

    Created a makeGrid function to display a 16x16 grid on a webpage

---
+++ script.js
@@ -0,0 +1,19 @@
+const container = document.querySelector('#container');
+
+function makeGrid(size) {
+    for(let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.className = 'gridRow'
+
+        for(let j = 0; j < size; j++) {
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+makeGrid(16);
+
+
