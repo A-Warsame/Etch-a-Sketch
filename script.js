62081130a42c3ee401fb07860cb1a7e869965465
A-Warsame <aawarsame12@gmail.com>
2024-08-04 13:53:59 +0100

    Moved the event listener oustide the makeGrid function and into it's own addHoverEffect function

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const container = document.querySelector('#container');
 
 function makeGrid(size) {
-    for(let i = 0; i < size; i++) {
+    for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
-        row.className = 'gridRow'
-
-        for(let j = 0; j < size; j++) {
+        row.className = 'gridRow';
+        for (let j = 0; j < size; j++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
-
-            cell.addEventListener("mouseover", function (e) {
-                e.target.style.background = "blue";
-            });
-            
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
 }
 
+function addHoverEffect() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener("mouseover", function(e) {
+            e.target.style.background = "blue";
+        });
+    });
+}
+
 makeGrid(16);
+addHoverEffect();
