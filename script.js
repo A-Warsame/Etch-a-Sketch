104b700042f3c7ece438f71617b403cf1bbb04fe
A-Warsame <aawarsame12@gmail.com>
2024-08-07 09:37:48 +0100

    Implemented a function to create a new grid with a prompt for the size when the Reset Grid button is pressed

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 const container = document.querySelector('#container');
 
 function makeGrid(size) {
+    container.innerHTML = '';
     for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
-        row.className = 'gridRow';
+        row.className = 'row';
         for (let j = 0; j < size; j++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
+            cell.addEventListener("mouseover", function(e) {
+                e.target.style.background = "blue";
+            });
             row.appendChild(cell);
         }
         container.appendChild(row);
     }
 }
 
-function addHoverEffect() {
-    const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => {
-        cell.addEventListener("mouseover", function(e) {
-            e.target.style.background = "blue";
-        });
-    });
-}
-
-makeGrid(16);
-addHoverEffect();
-
-function resetGridColor(){
+function resetGridColor() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => {
         cell.style.backgroundColor = "white";
     });
 }
+
+function newGrid() {
+    let number = prompt("How many squares per side for the new grid? (Max 100)");
+    number = parseInt(number);
+    if (number > 0 && number <= 100) {
+        makeGrid(number);
+    } else {
+        alert("Please choose a number between 1 and 100.");
+        newGrid();
+    }
+}
+
+makeGrid(16);
